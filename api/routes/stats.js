33b70c8048424745854ea8a46f3722585cbefa93
Beratsrkc <beratsarikoc6@gmail.com>
2025-09-06 17:30:03 +0300

    stats

---
+++ api/routes/stats.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const moment = require("moment");
+const Response = require("../lib/Response");
+const AuditLogs = require("../db/models/AuditLogs");
+const Categories = require("../db/models/Categories");
+const Users = require("../db/models/Users");
+const router = express.Router();
+const auth = require("../lib/auth")();
+
+router.all("*", auth.authenticate(), (req, res, next) => {
+  next();
+});
+
+/*
+1.Auditlogs tablosunda işlem yapan kişilerin hangi tip işlemi kaç kez yaptığını veren bir sorgu
+2.Kategori Tablosunda tekil veri sayısı
+3.Sistemde kayıtlı kaç kullanıcı var
+*/
+router.post("/auditlogs", async (req, res) => {
+  try {
+        let body = req.body;
+    let filter = {};
+
+    if (typeof body.is_active === "boolean") filter.is_active = body.is_active;
+    if (typeof body.location === "string") filter.location = body.location;
+    let result = await AuditLogs.aggregate([
+      { $match: filter },
+      {
+        $group: {
+          _id: { email: "$email", proc_type: "$proc_type" },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { coun: -1 } },
+    ]);
+
+    res.json(Response.successResponse(result));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error, req.user?.language);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/categories/unique", async (req, res) => {
+    try {
+
+        let body = req.body;
+        let filter = {};
+
+        if (typeof body.is_active === "boolean") filter.is_active = body.is_active;
+
+        let result = await Categories.distinct("name", filter);
+
+    res.json(Response.successResponse({ result, count: result.length }));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error, req.user?.language);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/users/count", async (req, res) => {
+  try {
+    let body = req.body;
+    let filter = {};
+
+    if (typeof body.is_active === "boolean") filter.is_active = body.is_active;
+
+    let result = await Users.countDocuments(filter);
+
+    res.json(Response.successResponse({ result, count: result.length }));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error, req.user?.language);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+module.exports = router;
