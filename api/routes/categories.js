bc91a81c0897a0f45790b2a3a8e35bacd9748aa0
Beratsrkc <beratsarikoc6@gmail.com>
2025-09-06 15:49:31 +0300

    import export

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -7,8 +7,27 @@ const Enum = require("../config/Enum");
 const AuditLogs = require("../lib/AuditLogs");
 const logger = require("../lib/logger/LoggerClass");
 const config = require("../config");
+const emitter = require("../lib/Emitter");
 const auth = require("../lib/auth")();
 const i18n = new (require("../lib/i18n"))(config.DEFAULT_LANG);
+const excelExport = new (require("../lib/Export"))();
+let fs = require("fs");
+const multer = require("multer");
+const path = require("path");
+const excelImport = new (require("../lib/Import"))();
+let multerStorage = multer.diskStorage({
+  destination: (req, file, next) => {
+    next(null, config.FILE_UPLOAD_PATH);
+  },
+  filename: (req, file, next) => {
+    next(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: multerStorage }).single("pb_file");
 /**
  *Create
  *Read
@@ -16,10 +35,10 @@ const i18n = new (require("../lib/i18n"))(config.DEFAULT_LANG);
  *Delete
  *CRUD
  */
-router.all("*",auth.authenticate(),(req,res,next)=>{
+router.all("*", auth.authenticate(), (req, res, next) => {
   next();
-})
-router.get("/", auth.checkRoles("category_view") , async (req, res) => {
+});
+router.get("/", auth.checkRoles("category_view"), async (req, res) => {
   try {
     let categories = await Categories.find({});
 
@@ -30,10 +49,17 @@ router.get("/", auth.checkRoles("category_view") , async (req, res) => {
   }
 });
 
-router.post("/add", auth.checkRoles("category_add") , async (req, res) => {
+router.post("/add", auth.checkRoles("category_add"), async (req, res) => {
   let body = req.body;
   try {
-    if (!body.name) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, i18n.translate("COMMON.VALIDATION_ERROR_TITLE", req.user.language), i18n.translate("COMMON.FIELD_MUST_BE_FILLED", req.user.language, ["name"]));
+    if (!body.name)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        i18n.translate("COMMON.VALIDATION_ERROR_TITLE", req.user.language),
+        i18n.translate("COMMON.FIELD_MUST_BE_FILLED", req.user.language, [
+          "name",
+        ])
+      );
 
     let category = new Categories({
       name: body.name,
@@ -43,20 +69,29 @@ router.post("/add", auth.checkRoles("category_add") , async (req, res) => {
     await category.save();
 
     AuditLogs.info(req.user?.email, "Categories", "Add", category);
-    logger.info(req.user?.email,"Categories","Add",category)
+    logger.info(req.user?.email, "Categories", "Add", category);
+    emitter
+      .getEmitter("notifications")
+      .emit("messages", { message: category.name + " is added" });
     res.json(Response.successResponse({ success: true }));
   } catch (error) {
-    logger.error(req.user?.email,"Categories","Add",error)
+    logger.error(req.user?.email, "Categories", "Add", error);
     let errorResponse = Response.errorResponse(error);
     res.status(errorResponse.code).json(errorResponse);
   }
 });
 
-router.post("/update",auth.checkRoles("category_update") , async (req, res) => {
+router.post("/update", auth.checkRoles("category_update"), async (req, res) => {
   let body = req.body;
   try {
-   if (!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, i18n.translate("COMMON.VALIDATION_ERROR_TITLE", req.user.language), i18n.translate("COMMON.FIELD_MUST_BE_FILLED", req.user.language, ["_id"]));
-
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        i18n.translate("COMMON.VALIDATION_ERROR_TITLE", req.user.language),
+        i18n.translate("COMMON.FIELD_MUST_BE_FILLED", req.user.language, [
+          "_id",
+        ])
+      );
 
     let updates = {};
 
@@ -77,14 +112,21 @@ router.post("/update",auth.checkRoles("category_update") , async (req, res) => {
   }
 });
 
-router.post("/delete",auth.checkRoles("category_delete") ,  async (req, res) => {
+router.post("/delete", auth.checkRoles("category_delete"), async (req, res) => {
   let body = req.body;
 
   try {
-    if (!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, i18n.translate("COMMON.VALIDATION_ERROR_TITLE", req.user.language), i18n.translate("COMMON.FIELD_MUST_BE_FILLED", req.user.language, ["_id"]));
+    if (!body._id)
+      throw new CustomError(
+        Enum.HTTP_CODES.BAD_REQUEST,
+        i18n.translate("COMMON.VALIDATION_ERROR_TITLE", req.user.language),
+        i18n.translate("COMMON.FIELD_MUST_BE_FILLED", req.user.language, [
+          "_id",
+        ])
+      );
 
     await Categories.deleteOne({ _id: body._id });
-    
+
     AuditLogs.info(req.user?.email, "Categories", "Delete", { _id: body._id });
     res.json(Response.successResponse({ success: true }));
   } catch (error) {
@@ -92,5 +134,56 @@ router.post("/delete",auth.checkRoles("category_delete") ,  async (req, res) => 
     res.status(errorResponse.code).json(errorResponse);
   }
 });
+
+router.post("/export", auth.checkRoles("category_export"), async (req, res) => {
+  try {
+    let categories = await Categories.find({});
+
+    let excel = excelExport.toExcel(
+      ["NAME", "IS ACTIVE?", "USER_ID", "CREATED AT", "UPDATED AT"],
+      ["name", "is_active", "created_by", "created_at", "updated_at"],
+      categories
+    );
+    let filePath =
+      __dirname + "/../tmp/categories_excel_" + Date.now() + ".xlsx";
+
+    fs.writeFileSync(filePath, excel, "UTF-8");
+
+    res.download(filePath);
+
+    //fs.unlinkSync(filePath);
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/import", auth.checkRoles("category_add"), upload, async (req, res) => {
+    try {
+
+        let file = req.file;
+        let body = req.body;
+
+        let rows = excelImport.fromExcel(file.path);
+
+        for (let i = 1; i < rows.length; i++) {
+            let [name, is_active, user, created_at, updated_at] = rows[i];
+            if (name) {
+                await Categories.create({
+                    name,
+                    is_active,
+                    created_by: req.user._id
+                });
+            }
+        }
+
+        res.status(Enum.HTTP_CODES.CREATED).json(Response.successResponse(req.body, Enum.HTTP_CODES.CREATED));
+
+    } catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(Response.errorResponse(err));
+    }
+})
+
 
 module.exports = router;
